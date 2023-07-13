675ba1e785853b080e975b59a3cd12a8c87bd8c6
nLowry292 <98676668+nLowry292@users.noreply.github.com>
2023-07-13 12:03:35 -0400

    Time: 34 ms (85.25%), Space: 13.6 MB (99.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        split_s = list(s)
-        split_t = list(t)
-        split_s.sort()
-        split_t.sort()
-        return split_s == split_t
+        if len(s) != len(t):
+            return False
+        chars = {}
+        for char in s:
+            chars[char] = chars.get(char, 0) + 1
+        for char in t:
+            chars[char] = chars.get(char, 0) - 1
+        for char in chars:
+            if chars[char] != 0:
+                return False
+        return True
