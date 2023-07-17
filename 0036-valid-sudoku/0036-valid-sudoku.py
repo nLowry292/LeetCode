8e00ca010ef2e46cbb5225bc7fb3668bfcaeaa60
nLowry292 <98676668+nLowry292@users.noreply.github.com>
2023-07-17 12:13:32 -0400

    Time: 121 ms (39.54%), Space: 16.2 MB (84.79%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        def containsDuplicate(lst: List[str]) -> bool:
+            seen = set()
+            for str in lst:
+                if str in seen:
+                    return True
+                
+                if str != ".":
+                    seen.add(str)
+            return False
+        
+        def boxDuplicate(board: List[List[str]], row: int, col: int) -> bool:
+            box = []
+            for i in range(0,3):
+                for j in range(0,3):
+                    box.append(board[row+i][col+j])
+            return containsDuplicate(box)
+
+        for row in board:
+            if containsDuplicate(row):
+                return False
+
+        col = ["."]*9
+        for i in range(0,9):
+            for j in range(0,9):
+                col[j]=board[j][i]
+            if containsDuplicate(col):
+                return False
+
+        for k in range(0,3):
+            for l in range(0,3):
+                if boxDuplicate(board, 3*k, 3*l):
+                    return False
+        
+        return True
+        
