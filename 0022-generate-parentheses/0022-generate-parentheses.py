5eba87676ed6c8a1a69f1ecf64cf3e3bf3b3c105
nLowry292 <98676668+nLowry292@users.noreply.github.com>
2023-07-25 22:02:42 -0400

    Time: 49 ms (66.74%), Space: 16.7 MB (28.46%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,39 +1,33 @@
 class Solution:
-    # def generateParenthesis(self, n: int) -> List[str]:
-    #     string = ""
-    #     res = []
-    #     def backtrack(openN: int , closedN: int):
-    #         global string
-    #         if openN == closedN == n:
-    #             res.append(string)
-    #         if openN < n:
-    #             "".join([string,'('])
-    #             backtrack(openN+1, closedN)
-    #             string = string[0:len(string)]
-    #         if closedN < openN:
-    #             string = string + ')'
-    #             backtrack(openN, closedN+1)
-    #             string = string[0:len(string)]
-    #     backtrack(0,0)
-    #     return res
-    
     def generateParenthesis(self, n: int) -> List[str]:
-        stack = []
         res = []
-
-        def backtrack(openN, closedN):
+        def backtrack(openN: int , closedN: int, string: str):
             if openN == closedN == n:
-                res.append("".join(stack))
-                #return
-
+                res.append(string)
             if openN < n:
-                stack.append("(")
-                backtrack(openN + 1, closedN)
-                stack.pop()
+                backtrack(openN+1, closedN, string+'(')
             if closedN < openN:
-                stack.append(")")
-                backtrack(openN, closedN + 1)
-                stack.pop()
-
-        backtrack(0, 0)
+                backtrack(openN, closedN+1, string+')')
+        backtrack(0,0, "")
         return res
+    
+#     def generateParenthesis(self, n: int) -> List[str]:
+#         stack = []
+#         res = []
+
+#         def backtrack(openN, closedN):
+#             if openN == closedN == n:
+#                 res.append("".join(stack))
+#                 return #this return stops you from running the next two checks, as they are redundant
+
+#             if openN < n:
+#                 stack.append("(")
+#                 backtrack(openN + 1, closedN)
+#                 stack.pop()
+#             if closedN < openN:
+#                 stack.append(")")
+#                 backtrack(openN, closedN + 1)
+#                 stack.pop()
+
+#         backtrack(0, 0)
+#         return res
